049fe606647488cd6bd541e4722ec3e9a91862a3
guna3005 <pranithreddyguna01@gmail.com>
2024-11-03 16:52:36 -0500

    adding basic styles and html buttons

--- app.js
+++ app.js
@@ -1,34 +1,26 @@
-// app.js
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-// Serve static files from the "public" directory
 app.use(express.static(__dirname + '/public'));
 
-// Route to serve the index.html file
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// Listen for client connections
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // Listen for chat messages
   socket.on('chat message', (msg) => {
-    // Broadcast the message to all connected clients
     io.emit('chat message', msg);
   });
 
-  // Handle user disconnect
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
 });
 
-// Start the server
 http.listen(3000, () => {
   console.log('Server is listening on *:3000');
 });
